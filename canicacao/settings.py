f1bae5b3d5cfe48a0de0c60a80babfbd7de5b97e
Erick Murillo <erickmurillo22@gmail.com>
2015-11-16 11:32:26 -0600

    ---

--- canicacao/settings.py
+++ canicacao/settings.py
@@ -68,10 +68,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-DEBUG = True
-
-THUMBNAIL_DEBUG = True 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
